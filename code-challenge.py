b9b63ff3c3d6a614fd8ea3394a37c5e96e61f8fc
Batuhan Balta <bthn.balta@gmail.com>
2020-09-30 23:46:41 -0700

    day 2

---
+++ code-challenge.py
@@ -0,0 +1,29 @@
+# from word2number import w2n
+# arr = [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
+# # Print out all of the numbers in the following array that are divisible by 3:
+
+# for i in range(len(arr)):
+#     if arr[i] % 3 == 0:
+#         print(arr[i])
+
+arr1 = [
+    "five",
+    "twenty six",
+    "nine hundred ninety nine",
+    "twelve",
+    "eighteen",
+
+    "one hundred one",
+    "fifty two",
+    "forty one",
+    "seventy seven",
+    "six",
+    "twelve",
+    "four",
+    "sixteen"
+]
+# Print out all of the numbers in the following array that are divisible by 3:
+j = []
+for i in range(len(arr1)):
+    arr1[i]
+    print(arr1[i])
